7092d4dbaf66692c7f740a85b56ff4513b18eb44
itssaimaKhan <samiakhan8754@gmail.com>
2025-01-06 16:55:21 +0500

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import Form from "./components/Form";
+
 function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello!</h1>
+    <main className="flex min-h-screen text-black flex-col items-center justify-between p-24">
+     <Form />
     </main>
   );
 }
